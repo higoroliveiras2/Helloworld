9c9a9080b92cbe1249787c73a0d40bd6533fd0f1
higor.losilva <higor.losilva@OSA0716338W11-1>
2025-08-23 08:01:56 -0300

    Adicionar arquivos de projeto.

---
+++ Aula 01.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Helloworld;
+
+class Program
+{
+    static void Main()
+    {
+
+        //Static void é para atribuir uma variavel 
+
+        Console.WriteLine("Hello, World");
+        String name = "João";
+        Console.WriteLine(name)
+
+    }
+           }
